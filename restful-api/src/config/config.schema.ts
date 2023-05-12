a04dd3118ed97a97d94c24c2cef478fcd91499c9
lanbery <lanbery@163.com>
2023-05-12 23:45:58 +0800

    upd: change api folder name

--- restful-api/src/config/config.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from 'joi';
-
-export const configValidationSchema = Joi.object({
-  STAGE: Joi.string().default(''),
-  PORT: Joi.number().default(3000).required(),
-  APP_PREFIX: Joi.string().default(''),
-});
